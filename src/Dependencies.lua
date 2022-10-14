69b64aefe39044495a118099ba1ebcd202aa68e3
lucasfacci <lucasfacci@protonmail.com>
2022-10-14 19:31:46 -0300

    first commit

---
+++ src/Dependencies.lua
@@ -0,0 +1,12 @@
+Class = require 'lib/class'
+Event = require 'lib/knife.event'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+
+require 'src/constants'
+require 'src/StateMachine'
+
+require 'src/states/BaseState'
+require 'src/states/StateStack'
+
+require 'src/states/game/StartState'
